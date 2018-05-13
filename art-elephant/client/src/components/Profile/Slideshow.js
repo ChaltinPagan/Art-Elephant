f93af5f09c897b2c1fbd8f500c40f6de3b0ac9a8
Chaltin Pagan <chaltinpagan@gmail.com>
2018-05-13 13:35:16 -0400

    Changing CSS for React Bootstrap Carousel. Still experimenting.

--- art-elephant/client/src/components/Profile/Slideshow.js
+++ art-elephant/client/src/components/Profile/Slideshow.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
+import scissors from './scissors.jpg';
+import dress from './RedDress.jpg';
+import green from './Green.jpg';
 
 class Slideshow extends Component {
     constructor() {
@@ -13,23 +16,23 @@ class Slideshow extends Component {
         return (
             <div className="slideshow">
             {/* <img alt="art" src="https://static1.squarespace.com/static/576e96ed2994cae381d021af/t/576e9eb959cc68de01607f44/1466865973436/Scissors?format=500w" /> */}
-            <Carousel interval={null}>
+            <Carousel interval={null} indicators={false}>
                 <Carousel.Item>
-                    <img height={500} alt="painting" src="https://static1.squarespace.com/static/576e96ed2994cae381d021af/t/576e9eb959cc68de01607f44/1466865973436/Scissors?format=500w" />
+                    <img height={300} alt="painting" src={scissors} />
                     <Carousel.Caption>
                     <h3>Scissors</h3>
                     <p>Egg tempera on panel</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img height={500} alt="painting" src="https://static1.squarespace.com/static/576e96ed2994cae381d021af/576e9751f5e231ddf3c2b309/576e9fe1cd0f68ce8dccc195/1495396422242/PaganChaltin_RedDress%282012%29_eggTemperaOnPanel_24x15in%28high-res%29.jpg?format=300w" />
+                    <img height={300} alt="painting" src={dress} />
                     <Carousel.Caption>
                     <h3>Green and Blue</h3>
                     <p>Egg tempera on panel</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img height={500} alt="painting" src="https://static1.squarespace.com/static/576e96ed2994cae381d021af/576e9751f5e231ddf3c2b309/576e9fedcd0f68ce8dccc1c6/1495396448242/PaganChaltin_GreenAndBlue%282012%29_EggTemperaOnPanel_20x20in%28high-res%29.jpg?format=300w" />
+                    <img height={300} alt="painting" src={green} />
                     <Carousel.Caption>
                     <h3>Red Dress</h3>
                     <p>Egg tempera on panel</p>
