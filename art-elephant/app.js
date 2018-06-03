fb3264633d108bf1a8d4e680f3e98f86228641ea
Chaltin Pagan <chaltinpagan@gmail.com>
2018-06-02 21:49:45 -0400

    Cleaning up backend routes.

--- art-elephant/app.js
+++ art-elephant/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const http = require('http');
 const app = express();
 const port = 8000;
 
@@ -16,33 +15,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true
-//   }));
-
 app.use('/users', users);
 app.use('/artists', artists);
 
 app.get('/', function (req, res, next) {
-    // res.send(
-    //     `Home
-        
-    //     <br/>GET users: /users
-    //     <br/>GET artists: /artists
-    //     <br/>GET single artist: /artists/:id`
-    // );
     res.sendFile(path.join(__dirname, 'home.html'));
 });
-
-//Expects to recieve the error, so err is first arg in function
-// app.get('*', (err, req, res) => {
-//     if (!err) {
-//         res.status(404).send('Error!');
-//     }
-
-//     res.status(500).send(`Error! Description: ${err}`);
-// });
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
--- art-elephant/routes/artists.js
+++ art-elephant/routes/artists.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/artist-queries');
 
-// router.get('/', function (req, res, next) {
-//     res.send('Home');
-// });
-
 router.get('/', db.getArtists);
 router.get('/:id', db.getSingleArtist);
-router.get('/images/:user_id', db.getArtistImages);
 router.post('/new', db.addArtistProfile);
 router.post('/:user_id', db.getArtistByUserID);
 router.put('/:id', db.updateArtist);
