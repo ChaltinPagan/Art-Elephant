601ac9bcb5cb059139e22c0d34427d24a1fdf3c7
Chaltin Pagan <chaltinpagan@gmail.com>
2018-06-06 19:28:31 -0400

    Localhost version

--- art-elephant/app.js
+++ art-elephant/app.js
@@ -3,7 +3,6 @@ const logger = require('morgan');
 const path = require('path');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const port = 8000;
 
